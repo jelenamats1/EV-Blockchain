3c479ae41a4bc628e5d36ad53c1a351ca0f7b99d
ga89wiw <jelena.pajic@tum.de>
2017-12-11 17:18:09 +0100

    using aggregator contract

--- mydapp/src/client/scripts/app.jsx
+++ mydapp/src/client/scripts/app.jsx
@@ -5,41 +5,98 @@ import {InputBond, HashBond, BButton, TransactionProgressLabel} from 'parity-rea
 import {Rspan, Rimg, ReactiveComponent} from 'oo7-react';
 
 
-const CounterABI = [
-	{"constant":false,"inputs":[{"name":"_option","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"type":"function"},
-	{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"hasVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},
-	{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"votes","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},
-	{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":true,"name":"option","type":"uint256"}],"name":"Voted","type":"event"}
+const EVProfileABI = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_option",
+        "type": "uint256"
+      }
+    ],
+    "name": "vote",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "hasVoted",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "votes",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "who",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "option",
+        "type": "uint256"
+      }
+    ],
+    "name": "Voted",
+    "type": "event"
+  }
 ];
+
 const Options = ['Red', 'Green', 'Blue'];
 
 export class App extends React.Component {
 	constructor () {
 		super();
-		this.counter = bonds.makeContract('0x7aC77Cb854E064f22E747F40b90FE6D6Bc1e3197', CounterABI);
+		this.aggregator = bonds.makeContract('0x4dF0e816F96CF064721289a7B04D85c3B9016891', EVProfileABI);
 		this.state = { tx: null };
 	}
 	render () {
-	return (<div>
-		{Options.map((n, i) => (<div key={i}>
-			<Rspan style={{
-				borderLeft: this.counter
-					.votes(i)
-					.map(v => `${1 + v * 10}px black solid`)
-			}}>
-			<a
-				style={{float: 'left', minWidth: '3em'}}
-				href='#'
-				onClick={() => this.setState({tx: this.counter.vote(i)})}
-				>
-				{n}
-			</a>
-			</Rspan>
-		</div>))}
-		<div style={{marginTop: '1em'}}>
-			<TransactionProgressLabel value={this.state.tx}/>
-		</div>
-	</div>);
+	return (
+		<div>
+		<Rspan>{bonds.height}</Rspan>
+		<br />
+		<BButton
+		content='Register'
+		onClick={() => this.aggregator.registerProfile()}
+		/>
+		
+	</div>
+);
 }
 }
 
